6c480107be87f713022be2543bfb1e14b9374eec
S. D <dinarigem@yahoo.fr>
2018-11-06 21:10:26 +0100

    PHP Warning "explode()
    fix of bug at line 127 : PHP Warning "explode() expects parameter 2 to be string, array given" for multi-value country fields

--- CRM/Core/BAO/CustomValueTable.php
+++ CRM/Core/BAO/CustomValueTable.php
@@ -124,7 +124,7 @@ class CRM_Core_BAO_CustomValueTable {
 
             case 'Country':
               $type = 'Integer';
-              $mulValues = explode(',', $value);
+              $mulValues =$value; //<SDI>$mulValues = explode(',', $value);
               if (is_array($value)) {
                 $value = CRM_Core_DAO::VALUE_SEPARATOR . implode(CRM_Core_DAO::VALUE_SEPARATOR, $value) . CRM_Core_DAO::VALUE_SEPARATOR;
                 $type = 'String';
